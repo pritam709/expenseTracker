970587b4325cde95b82fb91d3d2434969489f94c
Pravesh <pritamyadav70923@gmail.com>
2023-07-03 15:56:08 +0530

    update expense button

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,13 +1,20 @@
+import { useState } from "react"
 import "./ExpenseItem.css"
 const ExpenseDetails=(props)=>{
+    const [expense, setExpense]=useState(props.amount)
     const deleteExpense=()=>{
         
     }
+    const updateExpense=()=>{
+        setExpense("100");
+    }
+
     return <div className="expense-item__description">
     <h2>{props.title}</h2>
     <div>{props.location}</div>
-    <div className="expense-item__price">{props.amount}</div>
-    <button onClick={deleteExpense}>delete expenses</button>
+    <div className="expense-item__price">${expense}</div>
+    <button onClick={updateExpense}>update expenses</button>
+    {/* <button onClick={deleteExpense}>delete expenses</button> */}
 </div>
 }
 export default ExpenseDetails;
