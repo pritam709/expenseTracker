83e046078a4bc716620cd48eb02e9d91f93d7d91
Pravesh <pritamyadav70923@gmail.com>
2023-07-03 11:56:50 +0530

    event liseners

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,9 +1,13 @@
 import "./ExpenseItem.css"
 const ExpenseDetails=(props)=>{
+    const deleteExpense=()=>{
+        
+    }
     return <div className="expense-item__description">
     <h2>{props.title}</h2>
     <div>{props.location}</div>
     <div className="expense-item__price">{props.amount}</div>
+    <button onClick={deleteExpense}>delete expenses</button>
 </div>
 }
 export default ExpenseDetails;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,11 +4,12 @@ import "./Expenses.css"
 const Expenses=(props)=>{
 
     return <Card className="expenses"> {props.items.map(item=>{
-        return <ExpenseItem key={item.id}
+        return<><ExpenseItem key={item.id}
         title={item.title}
         amount={item.amount}
         location={item.location}
         date={item.date} />
+       </> 
       })}</Card>
    
     
