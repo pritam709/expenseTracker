55815c9f585ae716dd5f045e29c265b58cc511b7
Pravesh <pritamyadav70923@gmail.com>
2023-07-04 11:20:24 +0530

    submitting the form

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,35 +1,60 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
-    const inputChangeHandler=(event)=>{
+  const [initialState, setState] = useState({
+    title:"",
+    amount:"",
+    date:""
 
-        console.log(event.target.value);
-
+  });
+//   const [amount, setAmount] = useState("");
+ 
+//   const [date, setDate] = useState();
+   //    const [location,setLocation]=useState("");
+  const inputTitleHandler = (event) => {
+    setState({...initialState,title:event.target.value});
+    console.log(event.target.value);
+  };
+  const inputDateHandler = (event) => {
+    setState({...initialState,date:event.target.value});
+    console.log(event.target.value);
+  };
+  const inputAmountHandler = (event) => {
+    setState({...initialState,amount:event.target.value});
+    console.log(event.target.value);
+  };
+  const submitHandler=(event)=>{
+    event.preventDefault();
+    const obj= {
+     title: initialState.title,
+     amount:initialState.amount,
+     date:initialState.date
     }
+console.log(obj);
+  }
   return (
-    
-    <form className="new-expense" onChange={inputChangeHandler}>
+    <form className="new-expense" onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <label>Expense Title</label>
-        <input type="text"></input>
+        <input type="text" onChange={inputTitleHandler}></input>
       </div>
       <div className="new-expense__controls">
         <label>Expense Date</label>
-        <input type="date"></input>
+        <input type="date" onChange={inputDateHandler}></input>
       </div>
       <div className="new-expense__controls">
         <label>amount</label>
-        <input type="number"></input>
+        <input type="number" onChange={inputAmountHandler}></input>
       </div>
-      <div className="new-expense__controls">
+      {/* <div className="new-expense__controls">
         <label>location</label>
         <input type="text"></input>
-      </div>
+      </div> */}
 
       <div className="new-expense__actions ">
         <button type="submit">Submit</button>
       </div>
     </form>
-   
   );
 };
 export default ExpenseForm;
